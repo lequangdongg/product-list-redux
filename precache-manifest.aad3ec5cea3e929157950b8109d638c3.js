6e739768f132af244755292d75351f2af43a68d6
Quang Dong <lequangdongg@gmail.com>
2020-01-04 21:46:18 +0700

    Updates

---
+++ precache-manifest.aad3ec5cea3e929157950b8109d638c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08147a34bb77e4d6ca5c8ee356ab5f39",
+    "url": "/product-list-redux/index.html"
+  },
+  {
+    "revision": "38157664d605da02e5d4",
+    "url": "/product-list-redux/static/js/2.6c74cae0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/product-list-redux/static/js/2.6c74cae0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1409715bfb74395bd98",
+    "url": "/product-list-redux/static/js/main.147638b7.chunk.js"
+  },
+  {
+    "revision": "ae788d0535ee642c1816",
+    "url": "/product-list-redux/static/js/runtime-main.1175d153.js"
+  }
+]);
